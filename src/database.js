5e6f1c370fc028c08c8c6aeab4ca2193250f7ec5
Japneet Singh Chadha <japneetsingh560@gmail.com>
2023-11-06 19:05:57 +0530

    Delete src directory

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const {createPool}= require('mysql');
-const Pool=createPool({
-    host:"locathost",
-    user:"rooot",
-    password:"Raunak@123",
-    database:"details",
-    connectionLimit:20
-});
